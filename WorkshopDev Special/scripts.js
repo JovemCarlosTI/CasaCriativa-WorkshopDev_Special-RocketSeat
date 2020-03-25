2e2cc5ae3a5c1b37428c981bcb2445f2ff64c54e
Carlos André Martins da Silva <carlosandre116@outlook.com>
2020-03-25 14:33:58 -0300

    Casa Criativa

---
+++ WorkshopDev Special/scripts.js
@@ -0,0 +1,6 @@
+function onOff(){ // Monitora um evento de clique
+  document // No documento
+    .querySelector("#modal") // Procura pelo objeto com id footer
+    .classList // Recebe e reconhece as classes do documento
+    .toggle("hide") // Ativa a classe hide
+}
